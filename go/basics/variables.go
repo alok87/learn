4301c09c3ce2b2e6ab9257fdef5ed711d83700e8
Alok Kumar Singh <mail.alok87@gmail.com>
2014-12-08 18:07:07 +0530

    Create variables.go

---
+++ go/basics/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var variable_name_1 string = "value_var1"
+	var variable_name_2 int
+	fmt.Println(variable_name_1)
+	fmt.Println(variable_name_2)
+	
+	var a,b,c int = 1,2,3
+	fmt.Println("Values for a b c are=")
+	fmt.Println(a, b, c)
+	
+	d := "d=This is a shortcut to declare and define variables in GO"
+	fmt.Println(d)
+		
+}
