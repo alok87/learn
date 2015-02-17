812f5214f8f6a5694de3b4871529716105e161c3
Alok Kumar Singh <mail.alok87@gmail.com>
2015-02-17 22:05:30 +0530

    problem4 - largest 3 digit multiple palindrome

--- go/euler-problems/problem4.go
+++ go/euler-problems/problem4.go
@@ -3,17 +3,30 @@ package main
 import "fmt"
 import "os"
 
-func check_palidrome(n int) {
-	
+func check_palindrome(n int) int {
+	rev := 0
+	num := n
+	for num > 0 {
+		d := num % 10
+		rev = 10*rev + d
+		num /= 10
+	}
+	if n == rev {
+		return 1
+	} else {
+		return -1
+	}
+}
+
 func main() {
-	
-	var i,j,res,prod int
-	for i=999; i>0; i-- {
-		for j=i; j>0; j-- {
-			prod=i*j
-			res=check_palindrome(prod)
+	var i, j, res, prod int
+	for i = 999; i > 900; i-- {
+		for j = i; j > 900; j-- {
+			prod = i * j
+			res = check_palindrome(prod)
+			//fmt.Println(i,"*",j,"=",prod,res)
 			if res == 1 {
-				fmt.Println("Largest palindrome made from the product of two 3-digit numbers=",res)
+				fmt.Println("Largest palindrome made from the product of two 3-digit numbers=", prod)
 				os.Exit(0)
 			}
 		}
