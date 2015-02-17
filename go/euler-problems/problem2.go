ff8db992101566e7c155f2cc0740a4c2a80a3fed
Alok Kumar Singh <mail.alok87@gmail.com>
2015-02-17 15:30:54 +0530

    problem 2 - fibo series even valued term sum
    max term size 4million

---
+++ go/euler-problems/problem2.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func fibo(n int) (int) {
+	if n == 1 {
+		return 1
+	} else if n == 2 {
+		return 2
+	} else {
+		a := fibo(n - 1)
+		b := fibo(n - 2)
+		return a + b
+	}
+}
+
+func main() {
+	j := 0
+	sum := 0
+	for i:=1;j<=4000000; i++ {
+		j=fibo(i)
+		if j%2 == 0 {
+			sum+=j
+		}
+	}
+	fmt.Println("Total Sum=",sum)
+}
