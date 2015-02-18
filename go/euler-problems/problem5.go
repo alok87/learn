dfe352719baedeed973de3408c03f251050ae6fc
Alok Kumar Singh <mail.alok87@gmail.com>
2015-02-18 14:06:23 +0530

    problem5 - lcm for 1 to 20

--- go/euler-problems/problem5.go
+++ go/euler-problems/problem5.go
@@ -1,42 +1,26 @@
 package main
 
 import "fmt"
-import "math/big"
-
-func isPrime(n int64) int64 {
-	i := big.NewInt(n)
-	j := i.ProbablyPrime(1)
-	if j == false {
-		return -1
-	} else {
-		return 1
-	}
-}
 
 func lcm(a int64, b int64) int64 {
-	var isAPrime, isBPrime, max, flag_found int64
-	isAPrime = isPrime(a)
-	isBPrime = isPrime(b)
-	if isAPrime == 1 || isBPrime == 1 {
-		return a * b
-	}
-
+	var  max, flag_found int64
 	max = a
 	flag_found = 0
 	for flag_found == 0 {
 		if max%a == 0 && max%b == 0 {
 			flag_found=1
-		}
+		}else{
 		max += 1
+		}
 	}
-return max
+	return max
 }
 
 func main() {
 	var a, b, c int64
-	a = 20
+	a = 20 
 	b = 19
-	for b >= 11 {
+	for b >=11 {
 		c = lcm(a, b)
 		b = b - 1
 		a = c
