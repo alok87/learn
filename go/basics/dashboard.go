4bf6e1ebaf39afc62f6c488ce51868f79d56fff3
Alok Kumar Singh <mail.alok87@gmail.com>
2015-03-23 13:51:13 +0530

    Dashboard developed, under testing

--- go/basics/dashboard.go
+++ go/basics/dashboard.go
@@ -20,29 +20,78 @@ import "time"
 import "regexp"
 import "strings"
 import "bufio"
+import "path/filepath"
 
 // Golbal variables
-var projectDir string //The Home Directory for the Project, all data checked out here.
-var fileCSS string //The CSS file for HTML
-var bldDetailFile string // This the file TFS Build writes the current build informatio  after the build.
-var reportFile string // The dashboard report
+var projectDir string 									// The Home Directory for the Project, all data checked out here.
+var fileCSS,envs,apps string 							// The CSS file for HTML, the env and applications deployed list.
+var bldDetailFile string 								// This the file TFS Build writes the current build informatio  after the build.
+var reportFile,lastRunDate,endDate,lastDate string 		// The dashboard report and variables to store date between the report is created.
+var formatedStartDate,formatedEndDate,startDate string 	// Variables storing the formated date.
 
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+// Interface and Types Definations
+type CSS struct {}
+type Info struct {}
+type Checkin struct {}
+type Build struct {}
+type Environment struct {}
+type DashboardComponent interface {
+	createReport() string 
 }
 
-func writeFile(filename string, data []byte, perm os.FileMode) {
-	err := ioutil.WriteFile(filename,data,perm)
-	check(err)
+// Function Definations
+func (environment *Environment) createReport() string {
+	// This function creates the environment dashboard and returns the buffer
+	var buffer bytes.Buffer
+	buffer.WriteString("<table class=hovertable><tr><th class=top scope=col colspan=6>Environment Dashboard</th></tr><tr><th class=top scope=col>Environment</th><th class=top scope=col>Application</th><th class=top scope=col>Build-Deployed</th><th class=top scope=col>Start Time</th><th class=top scope=col>End-Time</th><th class=top scope=col>Manifest-deployed</th></tr>")
+	
+	env_list := strings.Split(envs," ")
+	app_list := strings.Split(apps," ")
+
+	for _,env := range env_list {
+		for _,app := range app_list {
+			buffer.WriteString("<tr><b><th>")
+			buffer.WriteString(env)
+			buffer.WriteString("</b></th><td>")
+			app_name := strings.Split(app,"\\envinfo_")
+			buffer.WriteString(app_name[1])
+			buffer.WriteString("</td>")
+
+			envDetailFile := filepath.Join(env,app)
+			fmt.Println(envDetailFile)
+
+			if _, err := os.Stat(envDetailFile); err == nil {
+				envDatafile, Openerr := os.Open(envDetailFile)
+				check(Openerr)
+				scanner := bufio.NewScanner(envDatafile)
+				for scanner.Scan() {
+    				lines := strings.Split(scanner.Text(),"#")
+
+    				full_bldLabel := strings.Split(lines[0],"\\")
+    				lines[0] = full_bldLabel[7]
+					for i:=0; i<len(lines); i++ {
+    					buffer.WriteString("<td colspan=1>")
+						buffer.WriteString(lines[i])
+						buffer.WriteString("</td>")
+					}
+					buffer.WriteString("</tr>")
+				}
+			} else {
+				buffer.WriteString("<td>-</td><td>Data-not-available</td><td>-</td><td>-</td>")
+				//os.Exit(1)
+			}
+			//buffer.WriteString("<td>-</td><td>-</td><td>-</td><td>-</td></tr>")
+		}
+
 }
 
-//func generateEnv(projectDir string) bytes.Buffer {
-//
-//}
 
-func generateBuild() bytes.Buffer {
+	buffer.WriteString("</table><br>")
+	return buffer.String()
+}
+
+func (build *Build) createReport() string {
+	// This function creates the build dashboard and returns the buffer
 	var buffer bytes.Buffer
 	buffer.WriteString("<table class=hovertable><tr><th class=top scope=col colspan=5>Build Dashboard</th></tr><tr><th class=top scope=col>Build-Label</th><th class=top scope=col>Build-Start-Time</th><th class=top scope=col>Build-Duration</th><th class=top scope=col>Build-Triggered-By</th><th class=top scope=col>Status</th></tr><tr>")
 	if _, err := os.Stat(bldDetailFile); err == nil {
@@ -67,14 +116,12 @@ func generateBuild() bytes.Buffer {
 	}else {
 		buffer.WriteString("<td>-</td><td>No-new-builds</td><td>-</td><td>-</td><td>-</td></tr>")
 	} 
-	buffer.WriteString("</table><br><br>")
-	return buffer
+	buffer.WriteString("</table><br>")
+	return buffer.String()
 }
 
-
-func generateCheckin(startDate string, endDate string) bytes.Buffer {
+func (checkin *Checkin) createReport() string {
 	var buffer,dateTime bytes.Buffer
-	var changeSet,dateCheckin,comments,changedBy,items string
 	buffer.WriteString("<table class=hovertable><tr><th class=top scope=col colspan=5>Checkin Dashboard</th></tr><tr><th class=top scope=col>Changeset</th><th class=top scope=col>Date</th><th class=top scope=col>Comments</th><th class=top scope=col>Changed by</th><th class=top scope=col>Items</th></tr><tr>")
 	dateTime.WriteString("/version:")
 	dateTime.WriteString(startDate)
@@ -91,40 +138,40 @@ func generateCheckin(startDate string, endDate string) bytes.Buffer {
 		removeLines := regexp.MustCompile("[-]+")
 		out = removeLines.ReplaceAllString(out, "")
 		lines := strings.Split(out,"Changeset")
-
 		for i:=1; i<len(lines); i++ {
-			rx_changeSet := regexp.MustCompile(`(: )(?P<middle>(.)+)(\nUser: )`)
-			changeSet = findMiddle(rx_changeSet,lines[i])
-			rx_dateCheckin := regexp.MustCompile(`(Date: )(?P<middle>(.)+)(\n)`)
-			dateCheckin = findMiddle(rx_dateCheckin,lines[i])
-			comments = getMiddle("Comment:","Items:",lines[i])
-			rx_changedBy := regexp.MustCompile(`(: )(?P<middle>(.)+)(\nDate: )`)
-			changedBy = findMiddle(rx_changedBy,lines[i])
-			items = getMiddle("Items:","Checkin Notes:",lines[i])
-
-			buffer.WriteString("<tr><td colspan=1>")
-			buffer.WriteString(changeSet)
-			buffer.WriteString("</td>")
-			
+			buffer.WriteString("<tr>")
+			buffer.WriteString(insertTableData(": ", "User:", lines[i]))
+			buffer.WriteString(insertTableData("Date: ", "Comment:", lines[i]))
+			buffer.WriteString(insertTableData("Comment:", "Items:", lines[i]))
+			rx_changedBy := regexp.MustCompile(`(User: )(?P<middle>(.)+)(\n)`)
+			changeset := findMiddle(rx_changedBy,lines[i])
 			buffer.WriteString("<td colspan=1>")
-			buffer.WriteString(dateCheckin)
+			buffer.WriteString(changeset)
 			buffer.WriteString("</td>")
-			
-			buffer.WriteString("<td colspan=1>")
-			buffer.WriteString(comments)
-			buffer.WriteString("</td>")
-	
-			buffer.WriteString("<td colspan=1>")
-			buffer.WriteString(changedBy)
-			buffer.WriteString("</td>")
-			
-			buffer.WriteString("<td colspan=1>")
-			buffer.WriteString(items)
-			buffer.WriteString("</td></tr>")
+			buffer.WriteString(insertTableData("Items:", "Checkin Notes:", lines[i]))
+			buffer.WriteString("</tr>")
 		}	
 		buffer.WriteString("</table><br>")
 	}
-	return buffer
+	return buffer.String()
+}
+
+func (info *Info) createReport() string {
+	var buffer bytes.Buffer
+	buffer.WriteString("<table class=hovertable><tr><th class=top scope=col colspan=2>Daily Report</th></tr><tr><th class=top scope=col>Cell</th><td align=center>OFS-15: Rewards OMS</td></tr><tr><th class=top scope=col>Starting</th><td align=center>")
+	buffer.WriteString(formatedStartDate)
+	buffer.WriteString("</td></tr><th class=top scope=col>Ending</th><td align=center>")
+	buffer.WriteString(formatedEndDate)
+	buffer.WriteString("</td></tr></table><br>")
+	return buffer.String()
+}
+
+func (css *CSS) createReport() string {
+		var buffer bytes.Buffer
+		data, err := ioutil.ReadFile(fileCSS)
+		check(err)
+		buffer.WriteString(string(data))
+		return buffer.String()
 }
 
 func findMiddle(myExp *regexp.Regexp,out string) string{
@@ -136,31 +183,27 @@ func findMiddle(myExp *regexp.Regexp,out string) string{
 		return result["middle"]
 }
 
+
+
 func getMiddle(start string, end string,out string) string {
+	if ! strings.Contains(out,end) {
+			return "-"
+	}
 	v1 := strings.Index(out,start)
 	v2 := strings.Index(out,end)
 	v1 = v1 + len(start)
-	v2 = v2 - len(end) + 1
+	//v2 = v2 - len(end) + 1
 	out = out[v1:v2]	
 	return out
 }
 
-func generateInfo(lastDate string,endDate string) bytes.Buffer {
+func insertTableData(from string, to string, line string) string {
+	tableData := getMiddle(from, to, line)
 	var buffer bytes.Buffer
-	buffer.WriteString("<table class=hovertable><tr><th class=top scope=col colspan=2>Daily Report: Rewards OMS</th></tr><tr><th class=top scope=col>Cell</th><td align=center>15: Rewards OMS</td></tr><tr><th class=top scope=col>Starting</th><td align=center>")
-	buffer.WriteString(lastDate)
-	buffer.WriteString("</td></tr><th class=top scope=col>Ending</th><td align=center>")
-	buffer.WriteString(endDate)
-	buffer.WriteString("</td></tr></table><br>")
-	return buffer
-}
-
-func generateCss() bytes.Buffer {
-		var buffer bytes.Buffer
-		data, err := ioutil.ReadFile(fileCSS)
-		check(err)
-		buffer.WriteString(string(data))
-		return buffer
+			buffer.WriteString("<td colspan=1>")
+			buffer.WriteString(tableData)
+			buffer.WriteString("</td>")
+	return buffer.String()
 }
 
 func getDate(startRunFile string) (string,string) {
@@ -186,44 +229,47 @@ func getDate(startRunFile string) (string,string) {
 		}
 }
 
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func writeFile(filename string, data []byte, perm os.FileMode) {
+	err := ioutil.WriteFile(filename,data,perm)
+	check(err)
+}
+
 func main() {	
-	var buffer_final bytes.Buffer // Buffer to store the Dashboard Report.
-	var lastRunFile,endRunFile string  // Used for calculating the duration in which the data need to be captured.
- 	now := time.Now()           // Variable to store the current time. Used for calculating the Total Execution time of the program.	       
+
+	var buffer_final bytes.Buffer 		// Buffer to store the Dashboard Report.
+	var lastRunFile,endRunFile string   // Used for calculating the duration in which the data need to be captured.
+ 	now := time.Now()           	    // Variable to store the current time. Used for calculating the Total Execution time of the program.	       
 	
 	// Parse Input - Can be overridden from command line. Eg: dashboard -home "my/customized/path" -lastRunFile "my/new/file"
-	flag.StringVar(&projectDir,"home","C:\\rewards_daily_dashboard","Home-Directory where the source is checked out?")
-	flag.StringVar(&lastRunFile,"lastRunFile","C:\\rewards_daily_dashboard\\lastRun.dat","Last Run File Path?")
-	flag.StringVar(&endRunFile,"endRunFile","C:\\rewards_daily_dashboard\\endRun.dat","End Run File Path?")
-	flag.StringVar(&reportFile,"reportFile","C:\\rewards_daily_dashboard\\report.html","Report File Path?")
-	flag.StringVar(&fileCSS,"cssFile","C:\\rewards_daily_dashboard\\report.css","CSS File Path?")
-	flag.StringVar(&bldDetailFile,"bldDetailFile","C:\\rewards_daily_dashboard\\buildDetail","TFS-Build-Info File Path?")
+	flag.StringVar(&projectDir,"home",".","Home-Directory where the source is checked out?")
+	flag.StringVar(&lastRunFile,"lastRunFile",".\\lastRun.dat","Last Run File Path?")
+	flag.StringVar(&endRunFile,"endRunFile",".\\endRun.dat","End Run File Path?")
+	flag.StringVar(&reportFile,"reportFile",".\\report.html","Report File Path?")
+	flag.StringVar(&fileCSS,"cssFile",".\\report.css","CSS File Path?")
+	flag.StringVar(&bldDetailFile,"bldDetailFile",".\\buildDetail","TFS-Build-Info File Path?")
+	flag.StringVar(&envs,"envs","DEV STG PPE","Environement Names")
+	flag.StringVar(&apps,"apps","\\envinfo_COM \\envinfo_MC","Application Names")
 	flag.Parse()
 
 	// Convert the start and end duration to TFS understood format. Used for calculating the duration to capture data.
-	startDate,formatedStartDate := getDate(lastRunFile)
-	endDate,formatedEndDate := getDate(endRunFile)
+	startDate,formatedStartDate = getDate(lastRunFile)
+	endDate,formatedEndDate = getDate(endRunFile)
 	
-    /* Different functions called to create different parts of the report. The report has 4 parts. 
-    	Info Dashboard
-    	Checkin Dashboard
-    	Build Dashobard
-    	Deployment Dashboard */
-	buffer_0 := generateCss()	
-	buffer_1 := generateInfo(formatedStartDate,formatedEndDate)
-	buffer_2 := generateCheckin(startDate,endDate)	
-	buffer_3 := generateBuild()
-	//buffer_4 := generateEnv()
-	
-	// Combine all the buffers to generate a final buffer containing the report.
-	buffer_final.WriteString(buffer_0.String())
-	buffer_final.WriteString(buffer_1.String())
-	buffer_final.WriteString(buffer_2.String())
-	buffer_final.WriteString(buffer_3.String())
-	//buffer_final.WriteString(buffer_4.String())
-	final_data := []byte(buffer_final.String())
+   // Generate the Reports and club the dashboard in one single buffer - buffer_final
+	dashboards := []DashboardComponent{&CSS{}, &Info{}, &Checkin{}, &Build{}, &Environment{}}
+	for _,dashboard :=  range dashboards {
+		buffer_final.WriteString(dashboard.createReport())
+	}
 
 	// Write the Final Buffer to the reportFile i.e. Dashboard report
+	buffer_final.WriteString("<p>Regards,<br><b>GEO-Products1 DevOps</p>")
+	final_data := []byte(buffer_final.String())
 	f,err := os.Create(reportFile)
 	check(err)
 	defer f.Close()
